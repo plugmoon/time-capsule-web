95e53170ca621c5bea2f5153dcb0c3b012e3d879
plugmoon <plugmoon@gmail.com>
2026-05-29 19:31:28 +0800

    Deploy time capsule web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.TIME_CAPSULE_FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCFz8XiosIIotjEHT4EfEfloGcQPVbX5ck",
+  authDomain: "time-capsule-web.firebaseapp.com",
+  projectId: "time-capsule-web",
+  storageBucket: "time-capsule-web.firebasestorage.app",
+  messagingSenderId: "638260344485",
+  appId: "1:638260344485:web:3197a57d91de3a626bff4c",
+  measurementId: "G-T98V486YTQ",
 };
